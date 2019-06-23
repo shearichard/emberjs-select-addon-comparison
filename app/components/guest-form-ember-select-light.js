4c29e06c8cc846bf20bb39ca819b70c4d3279848
Richard Shea <rshea@thecubagroup.com>
2019-06-23 23:27:08 +1200

    Resolved the issue which arises with ember-cp-validations where the users submits the form without having gone through each element and so the error messages are not shown

--- app/components/guest-form-ember-select-light.js
+++ app/components/guest-form-ember-select-light.js
@@ -6,6 +6,21 @@ export default Component.extend({
   myValue: 'turtle',
   myOptions: ['turtle', 'tortoise'],
 
+
+  //Each of the error display areas have their
+  //visibility controlled by a property in the 
+  //errMsgDspCntrl object. Their values are
+  //controlled by focus-out actions on each
+  //of the input elements
+  errMsgDspCntrl: {
+    nameError:null,
+    nationalityError:null,
+  },
+  //This name of errMsgDspCntrl is held in
+  //a variable for ease of use in error 
+  //messages
+  nameOfErrMsgDspCntrl : 'errMsgDspCntrl',
+
   actions:{
 
     myAction(event){
@@ -26,9 +41,23 @@ export default Component.extend({
           }
           else
           {
-            //Invoke all validations in order that
-            //any which fail validation will show
-            //their error message
+            //Iterate over validations.errors and use 'name'
+            //property to property the variable which controls
+            //the error display
+            validations.errors.forEach(function(error) {
+              let errCntrlPrpName = `${error.attribute}Error`;
+              let errCntrlPrpFullName = `${this.nameOfErrMsgDspCntrl}.${errCntrlPrpName}`;
+              if (this.errMsgDspCntrl.hasOwnProperty(errCntrlPrpName) == false)
+              {
+                let errMsg = "The property ${errCntrlPrpName} was not found in ${this.nameOfErrMsgDspCntrl} object. This is a programming error. Please report to the DRS system administrator." 
+                alert(errMsg);
+                throw(errMsg); 
+              }
+              else
+              {
+                this.set(errCntrlPrpFullName , true);
+              }
+            }, this);
           }
         })
     }
