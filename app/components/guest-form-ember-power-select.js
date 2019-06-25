44361ece4062d1e6f458e2ecbd6de1760c40dc41
Richard Shea <rshea@thecubagroup.com>
2019-06-25 13:10:18 +1200

    This has a working power-select, albeit with the wrong data source

--- app/components/guest-form-ember-power-select.js
+++ app/components/guest-form-ember-power-select.js
@@ -27,6 +27,11 @@ export default Component.extend({
 
   actions:{
 
+    searchRepo(term) {
+      let url = `https://api.github.com/search/repositories?q=${term}`;
+      return fetch(url).then((resp) => resp.json()).then((json) => json.items);
+    },
+
     nationalityChangeAction(event){
       //this.sendAction('changeNationalityHandler', event.target.value);
       alert("nationalityChangeAction has fired")
